27e0eac2381bcd7b60b8cd47a614957e8f094034
Brian Brogan <brian.brogan45@gmail.com>
2026-01-06 11:48:20 -0500

    bout machine skeleton

--- bout_machine.py
+++ bout_machine.py
@@ -34,17 +34,40 @@ class BoutMachine():
     def set_target_chain(self, target_chain: str):
         self.target_chain = target_chain
 
-    def set_video(self, video: pd.Series):
+    def set_video(self, video: pd.DataFrame):
         self.cur_video = video
     
-    def init_chain_acc(self, target_length):
-        self.chain_acc = target_length
+    #def get_chain_acc(self, target_length):
+    #    self.chain_acc = target_length
 
     def get_target_length(self):
         self.target_length = self.cur_video.shape[0]
 
-    def find_target(self):
-        pass
+    def run_machine(self):
+        #self.get_chain_acc()
+        self.chain_acc = 0
+        self.get_target_length()
+
+        cur_start = 0
+
+        
+        for idx, ref in enumerate(self.cur_video):
+            if self.use_filt_state and self.phase == 0:
+                # read the current grooming state in the video
+                state_read = ref['Filtered_State']
+                
+                # check if the current state is equal to the corresponding state in the target chain
+                if state_read == self.target_chain[self.chain_acc]:
+
+                    # check if the chain_acc was previously 0, if yes record start
+                    if self.chain_acc == 0:
+                        cur_start = ref['Start']
+
+
+
+
+
+            
 
 
     
