4e27850149193bf12806b7bd3f4ffa5522e4808b
Brian Brogan <brian.brogan45@gmail.com>
2026-01-07 13:53:27 -0500

    single video support

--- bout_machine.py
+++ bout_machine.py
@@ -37,9 +37,6 @@ class BoutMachine():
 
     def set_video(self, video: pd.DataFrame):
         self.cur_video = video
-    
-    #def get_chain_acc(self, target_length):
-    #    self.chain_acc = target_length
 
     def get_target_length(self):
         self.target_length = self.cur_video.shape[0]
@@ -52,59 +49,62 @@ class BoutMachine():
     def read_state(self, reference):
         state_read = None
         if self.use_filt_state:
-            state_read = reference['Filtered_State']
+            state_read = int(reference['Filtered_State'])
         else:
-            state_read = reference['Ordered_State'] 
+            state_read = int(reference['Ordered_State'])
 
         return state_read
     
 
-    def verify(self, state_read, target_chain, index):
+    def verify(self, state_read: int, target_chain, index):
 
-        if state_read == target_chain[index]:
+        target = int(target_chain[index])
+
+        if state_read == target:
             return True
         
         return False
+    
 
+    def run_machine(self, video: pd.DataFrame, verbose: bool = True):
 
+        cur_start = np.nan
+        cur_end = np.nan
+        identified = 0
+        log = {}
 
-    def run_machine(self):
-        #self.get_chain_acc()
-        #self.chain_acc = 0
-        # self.get_target_length() RUN INIT
-
-        cur_start = 0
-        cur_end = 0
-
-        for idx, ref in enumerate(self.cur_video):
+        for ref in video.iterrows():
             
             # read the current state in the video
-            state_read = self.read_state(ref)
+            state_read = self.read_state(ref[1])
+
+            if verbose:
+                print(f'state read: {state_read} chain acc: {self.chain_acc} target val: {self.target_chain[self.chain_acc]} verify: {self.verify(state_read, self.target_chain, self.chain_acc)}')
 
             # verify if the read state agrees with current index of target chain (BOOL)
             if self.verify(state_read, self.target_chain, self.chain_acc):
+                if self.chain_acc == 0:
+                    cur_start = ref[1]['Start']
                 self.chain_acc += 1
-                #cur_start = ref['Start']
-                cur_start = idx
+
+            # if not reset chain accumulator and start counter    
             else:
                 self.chain_acc = 0
+                cur_start = np.nan
 
             # add bout to return field if chain accumulator reaches max
-
             if self.chain_acc == len(self.target_chain):
-                pass
+                identified += 1
+                cur_end = ref[1]['End']
+                log[identified] = (cur_start, cur_end)
+
+                # then reset chain accumulator and counters
+                self.chain_acc = 0
+                cur_start = np.nan
+                cur_end = np.nan
+
+        return log
                 
-
-
-
-            # check if the current state is equal to the corresponding state in the target chain
-            if state_read == self.target_chain[self.chain_acc]:
-
-                # check if the chain_acc was previously 0, if yes record start
-                if self.chain_acc == 0:
-                    cur_start = ref['Start']
-                    
-                self.chain_acc += 1
 
 
 
