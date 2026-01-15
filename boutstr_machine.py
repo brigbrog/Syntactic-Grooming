8c14db4f5d25bbfc87220aebf4d9e6155dcf603f
Brian Brogan <brian.brogan45@gmail.com>
2026-01-15 10:51:25 -0500

    initial commit bout string machine

---
+++ boutstr_machine.py
@@ -0,0 +1,13 @@
+'''
+Brian Brogan
+Colby College - The Jackson Laboratory
+
+15 January 2025
+Grooming Bout Identifier (Bout string)
+------------------------
+'''
+
+import numpy as np
+import pandas as pd
+import os
+
